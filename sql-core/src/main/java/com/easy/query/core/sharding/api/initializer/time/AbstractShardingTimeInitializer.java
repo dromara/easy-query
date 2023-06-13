42df2c61a150a432c7e0798f89a0054102b14025
xuejiaming <326308290@qq.com>
2023-06-13 17:17:56 +0800

    分片初始化器支持分库分表如按年分库按月分表之类的

--- sql-core/src/main/java/com/easy/query/core/sharding/api/initializer/time/AbstractShardingTimeInitializer.java
+++ sql-core/src/main/java/com/easy/query/core/sharding/api/initializer/time/AbstractShardingTimeInitializer.java
@@ -16,7 +16,6 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.LinkedHashMap;
-import java.util.Map;
 
 /**
  * create time 2023/5/19 14:03
@@ -25,49 +24,101 @@ import java.util.Map;
  * @author xuejiaming
  */
 public abstract class AbstractShardingTimeInitializer<T> implements EntityShardingInitializer<T>, TimeJob {
-    public AbstractShardingTimeInitializer(){
+    public AbstractShardingTimeInitializer() {
         Type superClass = this.getClass().getGenericSuperclass();
         if (superClass instanceof Class) {
             throw new IllegalArgumentException("Internal error: TypeReference constructed without actual type information");
         } else {
-            this.entityClass = (Class<?>) ((ParameterizedType)superClass).getActualTypeArguments()[0];
+            this.entityClass = (Class<?>) ((ParameterizedType) superClass).getActualTypeArguments()[0];
         }
     }
+
     protected final Class<?> entityClass;
+
+    /**
+     * 分片起始时间,用于自动计算分片目前实际表,比如当前2023年如果你设置2021那么如果您是按年分片那么系统可以自动知道你现在有table_2021,table_2022,table_2023
+     *
+     * @return
+     */
     protected abstract LocalDateTime getBeginTime();
-    protected  LocalDateTime getEndTime(){
+
+    /**
+     * 结束时间不需要设置默认当前时间
+     *
+     * @return
+     */
+    protected LocalDateTime getEndTime() {
         return LocalDateTime.now();
     }
+
+    /**
+     * 开始时间的起始时间是多少,假设按天分表那么返回没有时分秒的时间,如果是按周那么就返回周一且没有时分秒,如果是按月就返回月初没有时分秒的时间
+     *
+     * @param beginTime
+     * @return
+     */
     protected abstract LocalDateTime getBeginTimeToStart(LocalDateTime beginTime);
+
+    /**
+     * 计算下次时间用于自动往系统内存中添加实际表名
+     *
+     * @param currentTime
+     * @return
+     */
     protected abstract LocalDateTime getNextTime(LocalDateTime currentTime);
+
+    /**
+     * 数据库表和尾巴的连接符,默认下划线可以为空
+     *
+     * @return
+     */
     protected String getTableSeparator() {
         return "_";
     }
+
+    /**
+     * 如何将时间格式化为数据库表名尾巴
+     *
+     * @param time
+     * @return
+     */
     protected abstract String formatTail(LocalDateTime time);
+
+    /**
+     * 根据时间格式化数据源名称
+     * @param time
+     * @return
+     */
+    protected String formatDataSource(LocalDateTime time,String defaultDataSource) {
+        return defaultDataSource;
+    }
+
     @Override
     public void configure(ShardingEntityBuilder<T> builder) {
         EntityMetadata entityMetadata = builder.getEntityMetadata();
         EasyQueryOption easyQueryOption = builder.getEasyQueryOption();
+        String defaultDataSourceName = easyQueryOption.getDefaultDataSourceName();
         String tableName = entityMetadata.getTableName();
         LocalDateTime setBeginTime = getBeginTime();
 
         LocalDateTime beginTime = getBeginTimeToStart(setBeginTime);
         LocalDateTime endTime = getEndTime();
-        if(beginTime.isAfter(endTime)){
-            throw new IllegalArgumentException("begin time:"+beginTime+" is after end time:"+endTime);
+        if (beginTime.isAfter(endTime)) {
+            throw new IllegalArgumentException("begin time:" + beginTime + " is after end time:" + endTime);
         }
         String tableSeparator = getTableSeparator();
 
-        ArrayList<String> actualTableNames = new ArrayList<>();
+        LinkedHashMap<String, Collection<String>> initTables = new LinkedHashMap<>();
         //应该是不在endTime后可以等于endTime
-        while(!beginTime.isAfter(endTime)){
+        while (!beginTime.isAfter(endTime)) {
+            String dataSource = formatDataSource(beginTime, defaultDataSourceName);
             String tail = formatTail(beginTime);
+            Collection<String> actualTableNames = initTables.computeIfAbsent(dataSource, key -> new ArrayList<>());
             actualTableNames.add(tableName + tableSeparator + tail);
             beginTime = getNextTime(beginTime);
         }
-        Map<String, Collection<String>> initTables = new LinkedHashMap<String, Collection<String>>() {{
-            put(easyQueryOption.getDefaultDataSourceName(), actualTableNames);
-        }};
+
+        //初始化实际表
         builder.actualTableNameInit(initTables);
         configure0(builder);
     }
@@ -91,16 +142,18 @@ public abstract class AbstractShardingTimeInitializer<T> implements EntityShardi
     public String jobName() {
         return EasyClassUtil.getSimpleName(getClass());
     }
+
     @Override
     public void execute(ServiceProvider serviceProvider) {
         EntityMetadataManager entityMetadataManager = serviceProvider.getService(EntityMetadataManager.class);
+        EasyQueryOption easyQueryOption = serviceProvider.getService(EasyQueryOption.class);
+        String defaultDataSourceName = easyQueryOption.getDefaultDataSourceName();
         EntityMetadata entityMetadata = entityMetadataManager.getEntityMetadata(entityClass);
         String tableName = entityMetadata.getTableName();
         LocalDateTime after5minutes = LocalDateTime.now().plusMinutes(5);
+        String dataSource = formatDataSource(after5minutes, defaultDataSourceName);
         String tail = formatTail(after5minutes);
         String actualTableName = tableName + getTableSeparator() + tail;
-        for (String dataSource : entityMetadata.getDataSources()) {
-            EasyDynamicUtil.addShardingEntity(entityMetadataManager,entityClass,dataSource,actualTableName);
-        }
+        EasyDynamicUtil.addShardingEntity(entityMetadataManager, entityClass, dataSource, actualTableName);
     }
 }
