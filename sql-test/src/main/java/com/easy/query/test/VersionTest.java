443dae3b310f2e7e8058b63016a8a961ea5e5035
xuejiaming <326308290@qq.com>
2023-07-31 10:30:53 +0800

    solon升级到2.4.1 默认无版本号更新将会报错,用户可以通过noVersionIgnore来忽略 noVersionError单元测试

--- sql-test/src/main/java/com/easy/query/test/VersionTest.java
+++ sql-test/src/main/java/com/easy/query/test/VersionTest.java
@@ -1,5 +1,6 @@
 package com.easy.query.test;
 
+import com.easy.query.core.exception.EasyQueryInvalidOperationException;
 import com.easy.query.test.entity.SysUserVersionLong;
 import com.easy.query.test.entity.SysUserVersionLongLogicDel;
 import org.junit.Assert;
@@ -15,130 +16,148 @@ import java.time.LocalDateTime;
  */
 public class VersionTest extends BaseTest {
     @Test
-    public void test1(){
-        String id="1";
+    public void test1() {
+        String id = "1";
         long l1 = easyQuery.deletable(SysUserVersionLong.class)
-                .whereById(id).executeRows();
+                .whereById(id).noVersionIgnore().executeRows();
         SysUserVersionLong sysUserVersionLong = new SysUserVersionLong();
         sysUserVersionLong.setId(id);
         sysUserVersionLong.setCreateTime(LocalDateTime.now());
         sysUserVersionLong.setVersion(1L);
-        sysUserVersionLong.setUsername("username"+id);
+        sysUserVersionLong.setUsername("username" + id);
         sysUserVersionLong.setPhone("13232323232");
         sysUserVersionLong.setIdCard("0000000000");
         sysUserVersionLong.setAddress("浙江省绍兴市越城区城市广场");
         long l = easyQuery.insertable(sysUserVersionLong).executeRows();
-        Assert.assertEquals(1,l);
+        Assert.assertEquals(1, l);
         SysUserVersionLong sysUserVersionLong1 = easyQuery.queryable(SysUserVersionLong.class)
                 .whereById(id).firstOrNull();
         Assert.assertNotNull(sysUserVersionLong1);
         long l2 = easyQuery.updatable(sysUserVersionLong1).executeRows();
-        Assert.assertEquals(1,l2);
+        Assert.assertEquals(1, l2);
     }
+
     @Test
-    public void test2(){
-        String id="2";
+    public void test2() {
+        String id = "2";
         long l1 = easyQuery.deletable(SysUserVersionLong.class)
-                .whereById(id).executeRows();
+                .whereById(id).noVersionIgnore().executeRows();
         SysUserVersionLong sysUserVersionLong = new SysUserVersionLong();
         sysUserVersionLong.setId(id);
         sysUserVersionLong.setCreateTime(LocalDateTime.now());
         sysUserVersionLong.setVersion(1L);
-        sysUserVersionLong.setUsername("username"+id);
+        sysUserVersionLong.setUsername("username" + id);
         sysUserVersionLong.setPhone("13232323232");
         sysUserVersionLong.setIdCard("0000000000");
         sysUserVersionLong.setAddress("浙江省绍兴市越城区城市广场");
         long l = easyQuery.insertable(sysUserVersionLong).executeRows();
-        Assert.assertEquals(1,l);
+        Assert.assertEquals(1, l);
 
+        {
+            try {
 
-        long l2 = easyQuery.updatable(SysUserVersionLong.class)
-                .set(SysUserVersionLong::getPhone, "123")
-                .whereById(id)
-                .executeRows();
-        Assert.assertEquals(1,l2);
+                long l2 = easyQuery.updatable(SysUserVersionLong.class)
+                        .set(SysUserVersionLong::getPhone, "123")
+                        .whereById(id)
+                        .executeRows();
+                Assert.assertEquals(1, l2);
+            } catch (Exception ex) {
+                Assert.assertTrue(ex instanceof EasyQueryInvalidOperationException);
+            }
+        }
+        {
+
+            long l2 = easyQuery.updatable(SysUserVersionLong.class)
+                    .set(SysUserVersionLong::getPhone, "123")
+                    .whereById(id)
+                    .noVersionIgnore()
+                    .executeRows();
+            Assert.assertEquals(1, l2);
+        }
         long l3 = easyQuery.updatable(SysUserVersionLong.class)
                 .set(SysUserVersionLong::getPhone, "123")
-                .where(o->o.eq(SysUserVersionLong::getId,id))
+                .where(o -> o.eq(SysUserVersionLong::getId, id))
+                .withVersion(1L)
                 .executeRows();
-        Assert.assertEquals(1,l3);
+        Assert.assertEquals(1, l3);
         long l4 = easyQuery.updatable(SysUserVersionLong.class)
                 .set(SysUserVersionLong::getPhone, "123")
-                .withVersion(1L)
-                .where(o->o.eq(SysUserVersionLong::getId,id))
+                .withVersion(2L)
+                .where(o -> o.eq(SysUserVersionLong::getId, id))
                 .executeRows();
-        Assert.assertEquals(1,l4);
-        long l5 = easyQuery.updatable(SysUserVersionLong.class)
-                .set(SysUserVersionLong::getPhone, "123")
-                .where(o->o.eq(SysUserVersionLong::getId,id))
-                .executeRows();
-        Assert.assertEquals(1,l5);
+        Assert.assertEquals(1, l4);
     }
+
     @Test
-    public void test3(){
-        String id="2";
+    public void test3() {
+        String id = "2";
         String s = easyQuery.updatable(SysUserVersionLong.class)
                 .set(SysUserVersionLong::getPhone, "123")
-                .whereById(id)
+                .whereById(id).noVersionIgnore()
                 .toSQL();
-        Assert.assertEquals("UPDATE `t_sys_user_version` SET `phone` = ? WHERE `id` = ?",s);
+        Assert.assertEquals("UPDATE `t_sys_user_version` SET `phone` = ? WHERE `id` = ?", s);
         String s1 = easyQuery.updatable(SysUserVersionLong.class)
-                .set(SysUserVersionLong::getPhone, "123")
+                .set(SysUserVersionLong::getPhone, "123").noVersionIgnore()
                 .where(o -> o.eq(SysUserVersionLong::getId, id)).toSQL();
-        Assert.assertEquals("UPDATE `t_sys_user_version` SET `phone` = ? WHERE `id` = ?",s1);
+        Assert.assertEquals("UPDATE `t_sys_user_version` SET `phone` = ? WHERE `id` = ?", s1);
         String s2 = easyQuery.updatable(SysUserVersionLong.class)
                 .set(SysUserVersionLong::getPhone, "123")
                 .withVersion(1L)
-                .where(o->o.eq(SysUserVersionLong::getId,id))
+                .noVersionError()
+                .where(o -> o.eq(SysUserVersionLong::getId, id))
                 .toSQL();
-        Assert.assertEquals("UPDATE `t_sys_user_version` SET `phone` = ?,`version` = ? WHERE `version` = ? AND `id` = ?",s2);
+        Assert.assertEquals("UPDATE `t_sys_user_version` SET `phone` = ?,`version` = ? WHERE `version` = ? AND `id` = ?", s2);
     }
+
     @Test
-    public void test4(){
-        String id="4";
+    public void test4() {
+        String id = "4";
         long l1 = easyQuery.deletable(SysUserVersionLongLogicDel.class)
-                .whereById(id).disableLogicDelete().executeRows();
+                .whereById(id).noVersionIgnore().disableLogicDelete().executeRows();
         SysUserVersionLongLogicDel sysUserVersionLongLogicDel = new SysUserVersionLongLogicDel();
         sysUserVersionLongLogicDel.setId(id);
         sysUserVersionLongLogicDel.setCreateTime(LocalDateTime.now());
         sysUserVersionLongLogicDel.setVersion(1L);
-        sysUserVersionLongLogicDel.setUsername("username"+id);
+        sysUserVersionLongLogicDel.setUsername("username" + id);
         sysUserVersionLongLogicDel.setPhone("13232323232");
         sysUserVersionLongLogicDel.setIdCard("0000000000");
         sysUserVersionLongLogicDel.setAddress("浙江省绍兴市越城区城市广场");
         sysUserVersionLongLogicDel.setDeleted(false);
         long l = easyQuery.insertable(sysUserVersionLongLogicDel).executeRows();
-        Assert.assertEquals(1,l);
+        Assert.assertEquals(1, l);
         long l2 = easyQuery.deletable(sysUserVersionLongLogicDel).executeRows();
-        Assert.assertEquals(1,l2);
+        Assert.assertEquals(1, l2);
     }
+
     @Test
-    public void test5(){
-        String id="5";
+    public void test5() {
+        String id = "5";
         long l1 = easyQuery.deletable(SysUserVersionLongLogicDel.class)
-                .whereById(id).disableLogicDelete().executeRows();
+                .whereById(id).noVersionIgnore().disableLogicDelete().executeRows();
         SysUserVersionLongLogicDel sysUserVersionLongLogicDel = new SysUserVersionLongLogicDel();
         sysUserVersionLongLogicDel.setId(id);
         sysUserVersionLongLogicDel.setCreateTime(LocalDateTime.now());
         sysUserVersionLongLogicDel.setVersion(1L);
-        sysUserVersionLongLogicDel.setUsername("username"+id);
+        sysUserVersionLongLogicDel.setUsername("username" + id);
         sysUserVersionLongLogicDel.setPhone("13232323232");
         sysUserVersionLongLogicDel.setIdCard("0000000000");
         sysUserVersionLongLogicDel.setAddress("浙江省绍兴市越城区城市广场");
         sysUserVersionLongLogicDel.setDeleted(false);
         long l = easyQuery.insertable(sysUserVersionLongLogicDel).executeRows();
-        Assert.assertEquals(1,l);
+        Assert.assertEquals(1, l);
         long l2 = easyQuery.deletable(SysUserVersionLongLogicDel.class)
+                .noVersionError()
                 .withVersion(1L)
                 .whereById(id).executeRows();
-        Assert.assertEquals(1,l2);
+        Assert.assertEquals(1, l2);
     }
+
     @Test
-    public void test6(){
-        String id="56";
-        String sql= easyQuery.deletable(SysUserVersionLongLogicDel.class)
+    public void test6() {
+        String id = "56";
+        String sql = easyQuery.deletable(SysUserVersionLongLogicDel.class)
                 .withVersion(1L)
                 .whereById(id).toSQL();
-        Assert.assertEquals("UPDATE `t_sys_user_version_del` SET `deleted` = ?,`version` = ? WHERE `deleted` = ? AND `version` = ? AND `id` = ?",sql);
+        Assert.assertEquals("UPDATE `t_sys_user_version_del` SET `deleted` = ?,`version` = ? WHERE `deleted` = ? AND `version` = ? AND `id` = ?", sql);
     }
 }
