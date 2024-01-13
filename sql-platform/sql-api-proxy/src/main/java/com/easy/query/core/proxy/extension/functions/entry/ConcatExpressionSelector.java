8f52c1017480287e456b4cea3fe3fb4c13d243ce
xuejiaming <326308290@qq.com>
2024-01-13 21:28:02 +0800

    优化concat函数

--- sql-platform/sql-api-proxy/src/main/java/com/easy/query/core/proxy/extension/functions/entry/ConcatExpressionSelector.java
+++ sql-platform/sql-api-proxy/src/main/java/com/easy/query/core/proxy/extension/functions/entry/ConcatExpressionSelector.java
@@ -4,22 +4,22 @@ import com.easy.query.core.proxy.PropTypeColumn;
 
 /**
  * create time 2024/1/13 21:20
- * 文件说明
+ * concat函数表达式选择器
  *
  * @author xuejiaming
  */
 public interface ConcatExpressionSelector {
     /**
      * 可以是值
-     * @param val
-     * @return
+     * @param val 值
+     * @return 返回当前选择器
      */
     ConcatExpressionSelector concatWith(String val);
 
     /**
      * 可以是函数也可以是列
-     * @param propTypeColumn
-     * @return
+     * @param propTypeColumn 列或者函数
+     * @return 返回当前选择器
      */
     ConcatExpressionSelector concatWith(PropTypeColumn<String> propTypeColumn);
 }
