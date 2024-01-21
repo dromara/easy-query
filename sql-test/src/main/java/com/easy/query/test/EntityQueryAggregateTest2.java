f9d2164c0ff0c1d5eb598774060e6ff415377d6e
xuejiaming <326308290@qq.com>
2024-01-21 09:35:37 +0800

    添加aggregate单元测试 抽象单测 1.9.17

--- sql-test/src/main/java/com/easy/query/test/EntityQueryAggregateTest2.java
+++ sql-test/src/main/java/com/easy/query/test/EntityQueryAggregateTest2.java
@@ -56,6 +56,326 @@ public class EntityQueryAggregateTest2 extends BaseEntityQueryAggregateTest1{
             Assert.assertEquals("1(String),1(String),%xx%(String)", EasySQLUtil.sqlParameterToString(jdbcExecuteAfterArg.getBeforeArg().getSqlParameters().get(0)));
             listenerContextManager.clear();
         }
+    }
+    @Test
+    public void test2() {
+        {
+            ListenerContext listenerContext = new ListenerContext();
+            listenerContextManager.startListen(listenerContext);
+            List<Topic> list = join2().whereById("1")
+                    .whereMerge(o -> {
+                        o.t1.id().eq("1");
+                        o.t2.id().eq(false, "1");
+                    })
+                    .where((t1, t2, t3, t4, t5, t6, t7, t8, t9) -> {
+                        t2.title().like("xx");
+                    })
+                    .where(false, (t1, t2, t3, t4, t5, t6, t7, t8, t9) -> {
+                        t2.title().like("xx");
+                    })
+                    .whereMerge(false, o -> {
+                        o.t1.id().eq("xx");
+                    })
+                    .orderBy((t1, t2, t3, t4, t5, t6, t7, t8, t9) -> {
+                        t1.createTime().asc();
+                        t2.createTime().desc(false);
+                    })
+                    .orderBy(false, (t1, t2, t3, t4, t5, t6, t7, t8, t9) -> {
+                        t1.createTime().asc();
+                        t2.createTime().desc(false);
+                    })
+                    .orderByMerge(o -> {
+                        o.t4.createTime().desc();
+                    })
+                    .orderByMerge(false, o -> {
+                        o.t4.createTime().asc();
+                    }).toList();
+            Assert.assertNotNull(listenerContext.getJdbcExecuteAfterArg());
+            JdbcExecuteAfterArg jdbcExecuteAfterArg = listenerContext.getJdbcExecuteAfterArg();
+            Assert.assertEquals("SELECT t.`id`,t.`stars`,t.`title`,t.`create_time` FROM `t_topic` t LEFT JOIN `t_topic` t1 ON t.`id` = t1.`id` LEFT JOIN `t_topic` t2 ON t.`id` = t2.`id` LEFT JOIN `t_topic` t3 ON t.`id` = t3.`id` LEFT JOIN `t_topic` t4 ON t.`id` = t4.`id` LEFT JOIN `t_topic` t5 ON t.`id` = t5.`id` LEFT JOIN `t_topic` t6 ON t.`id` = t6.`id` LEFT JOIN `t_topic` t7 ON t.`id` = t7.`id` LEFT JOIN `t_topic` t8 ON t.`id` = t8.`id` WHERE t.`id` = ? AND t.`id` = ? AND t1.`title` LIKE ? ORDER BY t.`create_time` ASC,t3.`create_time` DESC", jdbcExecuteAfterArg.getBeforeArg().getSql());
+            Assert.assertEquals("1(String),1(String),%xx%(String)", EasySQLUtil.sqlParameterToString(jdbcExecuteAfterArg.getBeforeArg().getSqlParameters().get(0)));
+            listenerContextManager.clear();
+        }
+    }
+    @Test
+    public void test3() {
+        {
+            ListenerContext listenerContext = new ListenerContext();
+            listenerContextManager.startListen(listenerContext);
+            List<Topic> list = join3().whereById("1")
+                    .whereMerge(o -> {
+                        o.t1.id().eq("1");
+                        o.t2.id().eq(false, "1");
+                    })
+                    .where((t1, t2, t3, t4, t5, t6, t7, t8) -> {
+                        t2.title().like("xx");
+                    })
+                    .where(false, (t1, t2, t3, t4, t5, t6, t7, t8) -> {
+                        t2.title().like("xx");
+                    })
+                    .whereMerge(false, o -> {
+                        o.t1.id().eq("xx");
+                    })
+                    .orderBy((t1, t2, t3, t4, t5, t6, t7, t8) -> {
+                        t1.createTime().asc();
+                        t2.createTime().desc(false);
+                    })
+                    .orderBy(false, (t1, t2, t3, t4, t5, t6, t7, t8) -> {
+                        t1.createTime().asc();
+                        t2.createTime().desc(false);
+                    })
+                    .orderByMerge(o -> {
+                        o.t4.createTime().desc();
+                    })
+                    .orderByMerge(false, o -> {
+                        o.t4.createTime().asc();
+                    }).toList();
+            Assert.assertNotNull(listenerContext.getJdbcExecuteAfterArg());
+            JdbcExecuteAfterArg jdbcExecuteAfterArg = listenerContext.getJdbcExecuteAfterArg();
+            Assert.assertEquals("SELECT t.`id`,t.`stars`,t.`title`,t.`create_time` FROM `t_topic` t LEFT JOIN `t_topic` t1 ON t.`id` = t1.`id` LEFT JOIN `t_topic` t2 ON t.`id` = t2.`id` LEFT JOIN `t_topic` t3 ON t.`id` = t3.`id` LEFT JOIN `t_topic` t4 ON t.`id` = t4.`id` LEFT JOIN `t_topic` t5 ON t.`id` = t5.`id` LEFT JOIN `t_topic` t6 ON t.`id` = t6.`id` LEFT JOIN `t_topic` t7 ON t.`id` = t7.`id` WHERE t.`id` = ? AND t.`id` = ? AND t1.`title` LIKE ? ORDER BY t.`create_time` ASC,t3.`create_time` DESC", jdbcExecuteAfterArg.getBeforeArg().getSql());
+            Assert.assertEquals("1(String),1(String),%xx%(String)", EasySQLUtil.sqlParameterToString(jdbcExecuteAfterArg.getBeforeArg().getSqlParameters().get(0)));
+            listenerContextManager.clear();
+        }
+    }
 
+    @Test
+    public void test4() {
+        {
+            ListenerContext listenerContext = new ListenerContext();
+            listenerContextManager.startListen(listenerContext);
+            List<Topic> list = join4().whereById("1")
+                    .whereMerge(o -> {
+                        o.t1.id().eq("1");
+                        o.t2.id().eq(false, "1");
+                    })
+                    .where((t1, t2, t3, t4, t5, t6, t7) -> {
+                        t2.title().like("xx");
+                    })
+                    .where(false, (t1, t2, t3, t4, t5, t6, t7) -> {
+                        t2.title().like("xx");
+                    })
+                    .whereMerge(false, o -> {
+                        o.t1.id().eq("xx");
+                    })
+                    .orderBy((t1, t2, t3, t4, t5, t6, t7) -> {
+                        t1.createTime().asc();
+                        t2.createTime().desc(false);
+                    })
+                    .orderBy(false, (t1, t2, t3, t4, t5, t6, t7) -> {
+                        t1.createTime().asc();
+                        t2.createTime().desc(false);
+                    })
+                    .orderByMerge(o -> {
+                        o.t4.createTime().desc();
+                    })
+                    .orderByMerge(false, o -> {
+                        o.t4.createTime().asc();
+                    }).toList();
+            Assert.assertNotNull(listenerContext.getJdbcExecuteAfterArg());
+            JdbcExecuteAfterArg jdbcExecuteAfterArg = listenerContext.getJdbcExecuteAfterArg();
+            Assert.assertEquals("SELECT t.`id`,t.`stars`,t.`title`,t.`create_time` FROM `t_topic` t LEFT JOIN `t_topic` t1 ON t.`id` = t1.`id` LEFT JOIN `t_topic` t2 ON t.`id` = t2.`id` LEFT JOIN `t_topic` t3 ON t.`id` = t3.`id` LEFT JOIN `t_topic` t4 ON t.`id` = t4.`id` LEFT JOIN `t_topic` t5 ON t.`id` = t5.`id` LEFT JOIN `t_topic` t6 ON t.`id` = t6.`id` WHERE t.`id` = ? AND t.`id` = ? AND t1.`title` LIKE ? ORDER BY t.`create_time` ASC,t3.`create_time` DESC", jdbcExecuteAfterArg.getBeforeArg().getSql());
+            Assert.assertEquals("1(String),1(String),%xx%(String)", EasySQLUtil.sqlParameterToString(jdbcExecuteAfterArg.getBeforeArg().getSqlParameters().get(0)));
+            listenerContextManager.clear();
+        }
+    }
+    @Test
+    public void test5() {
+        {
+            ListenerContext listenerContext = new ListenerContext();
+            listenerContextManager.startListen(listenerContext);
+            List<Topic> list = join5().whereById("1")
+                    .whereMerge(o -> {
+                        o.t1.id().eq("1");
+                        o.t2.id().eq(false, "1");
+                    })
+                    .where((t1, t2, t3, t4, t5, t6) -> {
+                        t2.title().like("xx");
+                    })
+                    .where(false, (t1, t2, t3, t4, t5, t6) -> {
+                        t2.title().like("xx");
+                    })
+                    .whereMerge(false, o -> {
+                        o.t1.id().eq("xx");
+                    })
+                    .orderBy((t1, t2, t3, t4, t5, t6) -> {
+                        t1.createTime().asc();
+                        t2.createTime().desc(false);
+                    })
+                    .orderBy(false, (t1, t2, t3, t4, t5, t6) -> {
+                        t1.createTime().asc();
+                        t2.createTime().desc(false);
+                    })
+                    .orderByMerge(o -> {
+                        o.t4.createTime().desc();
+                    })
+                    .orderByMerge(false, o -> {
+                        o.t4.createTime().asc();
+                    }).toList();
+            Assert.assertNotNull(listenerContext.getJdbcExecuteAfterArg());
+            JdbcExecuteAfterArg jdbcExecuteAfterArg = listenerContext.getJdbcExecuteAfterArg();
+            Assert.assertEquals("SELECT t.`id`,t.`stars`,t.`title`,t.`create_time` FROM `t_topic` t LEFT JOIN `t_topic` t1 ON t.`id` = t1.`id` LEFT JOIN `t_topic` t2 ON t.`id` = t2.`id` LEFT JOIN `t_topic` t3 ON t.`id` = t3.`id` LEFT JOIN `t_topic` t4 ON t.`id` = t4.`id` LEFT JOIN `t_topic` t5 ON t.`id` = t5.`id` WHERE t.`id` = ? AND t.`id` = ? AND t1.`title` LIKE ? ORDER BY t.`create_time` ASC,t3.`create_time` DESC", jdbcExecuteAfterArg.getBeforeArg().getSql());
+            Assert.assertEquals("1(String),1(String),%xx%(String)", EasySQLUtil.sqlParameterToString(jdbcExecuteAfterArg.getBeforeArg().getSqlParameters().get(0)));
+            listenerContextManager.clear();
+        }
+    }
+    @Test
+    public void test6() {
+        {
+            ListenerContext listenerContext = new ListenerContext();
+            listenerContextManager.startListen(listenerContext);
+            List<Topic> list = join6().whereById("1")
+                    .whereMerge(o -> {
+                        o.t1.id().eq("1");
+                        o.t2.id().eq(false, "1");
+                    })
+                    .where((t1, t2, t3, t4, t5) -> {
+                        t2.title().like("xx");
+                    })
+                    .where(false, (t1, t2, t3, t4, t5) -> {
+                        t2.title().like("xx");
+                    })
+                    .whereMerge(false, o -> {
+                        o.t1.id().eq("xx");
+                    })
+                    .orderBy((t1, t2, t3, t4, t5) -> {
+                        t1.createTime().asc();
+                        t2.createTime().desc(false);
+                    })
+                    .orderBy(false, (t1, t2, t3, t4, t5) -> {
+                        t1.createTime().asc();
+                        t2.createTime().desc(false);
+                    })
+                    .orderByMerge(o -> {
+                        o.t4.createTime().desc();
+                    })
+                    .orderByMerge(false, o -> {
+                        o.t4.createTime().asc();
+                    }).toList();
+            Assert.assertNotNull(listenerContext.getJdbcExecuteAfterArg());
+            JdbcExecuteAfterArg jdbcExecuteAfterArg = listenerContext.getJdbcExecuteAfterArg();
+            Assert.assertEquals("SELECT t.`id`,t.`stars`,t.`title`,t.`create_time` FROM `t_topic` t LEFT JOIN `t_topic` t1 ON t.`id` = t1.`id` LEFT JOIN `t_topic` t2 ON t.`id` = t2.`id` LEFT JOIN `t_topic` t3 ON t.`id` = t3.`id` LEFT JOIN `t_topic` t4 ON t.`id` = t4.`id` WHERE t.`id` = ? AND t.`id` = ? AND t1.`title` LIKE ? ORDER BY t.`create_time` ASC,t3.`create_time` DESC", jdbcExecuteAfterArg.getBeforeArg().getSql());
+            Assert.assertEquals("1(String),1(String),%xx%(String)", EasySQLUtil.sqlParameterToString(jdbcExecuteAfterArg.getBeforeArg().getSqlParameters().get(0)));
+            listenerContextManager.clear();
+        }
+    }
+    @Test
+    public void test7() {
+        {
+            ListenerContext listenerContext = new ListenerContext();
+            listenerContextManager.startListen(listenerContext);
+            List<Topic> list = join7().whereById("1")
+                    .whereMerge(o -> {
+                        o.t1.id().eq("1");
+                        o.t2.id().eq(false, "1");
+                    })
+                    .where((t1, t2, t3, t4) -> {
+                        t2.title().like("xx");
+                    })
+                    .where(false, (t1, t2, t3, t4) -> {
+                        t2.title().like("xx");
+                    })
+                    .whereMerge(false, o -> {
+                        o.t1.id().eq("xx");
+                    })
+                    .orderBy((t1, t2, t3, t4) -> {
+                        t1.createTime().asc();
+                        t2.createTime().desc(false);
+                    })
+                    .orderBy(false, (t1, t2, t3, t4) -> {
+                        t1.createTime().asc();
+                        t2.createTime().desc(false);
+                    })
+                    .orderByMerge(o -> {
+                        o.t4.createTime().desc();
+                    })
+                    .orderByMerge(false, o -> {
+                        o.t4.createTime().asc();
+                    }).toList();
+            Assert.assertNotNull(listenerContext.getJdbcExecuteAfterArg());
+            JdbcExecuteAfterArg jdbcExecuteAfterArg = listenerContext.getJdbcExecuteAfterArg();
+            Assert.assertEquals("SELECT t.`id`,t.`stars`,t.`title`,t.`create_time` FROM `t_topic` t LEFT JOIN `t_topic` t1 ON t.`id` = t1.`id` LEFT JOIN `t_topic` t2 ON t.`id` = t2.`id` LEFT JOIN `t_topic` t3 ON t.`id` = t3.`id` WHERE t.`id` = ? AND t.`id` = ? AND t1.`title` LIKE ? ORDER BY t.`create_time` ASC,t3.`create_time` DESC", jdbcExecuteAfterArg.getBeforeArg().getSql());
+            Assert.assertEquals("1(String),1(String),%xx%(String)", EasySQLUtil.sqlParameterToString(jdbcExecuteAfterArg.getBeforeArg().getSqlParameters().get(0)));
+            listenerContextManager.clear();
+        }
+    }
+    @Test
+    public void test8() {
+        {
+            ListenerContext listenerContext = new ListenerContext();
+            listenerContextManager.startListen(listenerContext);
+            List<Topic> list = join8().whereById("1")
+                    .whereMerge(o -> {
+                        o.t1.id().eq("1");
+                        o.t2.id().eq(false, "1");
+                    })
+                    .where((t1, t2, t3) -> {
+                        t2.title().like("xx");
+                    })
+                    .where(false, (t1, t2, t3) -> {
+                        t2.title().like("xx");
+                    })
+                    .whereMerge(false, o -> {
+                        o.t1.id().eq("xx");
+                    })
+                    .orderBy((t1, t2, t3) -> {
+                        t1.createTime().asc();
+                        t2.createTime().desc(false);
+                    })
+                    .orderBy(false, (t1, t2, t3) -> {
+                        t1.createTime().asc();
+                        t2.createTime().desc(false);
+                    })
+                    .orderByMerge(o -> {
+                        o.t3.createTime().desc();
+                    })
+                    .orderByMerge(false, o -> {
+                        o.t3.createTime().asc();
+                    }).toList();
+            Assert.assertNotNull(listenerContext.getJdbcExecuteAfterArg());
+            JdbcExecuteAfterArg jdbcExecuteAfterArg = listenerContext.getJdbcExecuteAfterArg();
+            Assert.assertEquals("SELECT t.`id`,t.`stars`,t.`title`,t.`create_time` FROM `t_topic` t LEFT JOIN `t_topic` t1 ON t.`id` = t1.`id` LEFT JOIN `t_topic` t2 ON t.`id` = t2.`id` WHERE t.`id` = ? AND t.`id` = ? AND t1.`title` LIKE ? ORDER BY t.`create_time` ASC,t2.`create_time` DESC", jdbcExecuteAfterArg.getBeforeArg().getSql());
+            Assert.assertEquals("1(String),1(String),%xx%(String)", EasySQLUtil.sqlParameterToString(jdbcExecuteAfterArg.getBeforeArg().getSqlParameters().get(0)));
+            listenerContextManager.clear();
+        }
+    }
+    @Test
+    public void test9() {
+        {
+            ListenerContext listenerContext = new ListenerContext();
+            listenerContextManager.startListen(listenerContext);
+            List<Topic> list = join9().whereById("1")
+                    .whereMerge(o -> {
+                        o.t1.id().eq("1");
+                        o.t2.id().eq(false, "1");
+                    })
+                    .where((t1, t2) -> {
+                        t2.title().like("xx");
+                    })
+                    .where(false, (t1, t2) -> {
+                        t2.title().like("xx");
+                    })
+                    .whereMerge(false, o -> {
+                        o.t1.id().eq("xx");
+                    })
+                    .orderBy((t1, t2) -> {
+                        t1.createTime().asc();
+                        t2.createTime().desc(false);
+                    })
+                    .orderBy(false, (t1, t2) -> {
+                        t1.createTime().asc();
+                        t2.createTime().desc(false);
+                    })
+                    .orderByMerge(o -> {
+                        o.t2.createTime().desc();
+                    })
+                    .orderByMerge(false, o -> {
+                        o.t2.createTime().asc();
+                    }).toList();
+            Assert.assertNotNull(listenerContext.getJdbcExecuteAfterArg());
+            JdbcExecuteAfterArg jdbcExecuteAfterArg = listenerContext.getJdbcExecuteAfterArg();
+            Assert.assertEquals("SELECT t.`id`,t.`stars`,t.`title`,t.`create_time` FROM `t_topic` t LEFT JOIN `t_topic` t1 ON t.`id` = t1.`id` WHERE t.`id` = ? AND t.`id` = ? AND t1.`title` LIKE ? ORDER BY t.`create_time` ASC,t1.`create_time` DESC", jdbcExecuteAfterArg.getBeforeArg().getSql());
+            Assert.assertEquals("1(String),1(String),%xx%(String)", EasySQLUtil.sqlParameterToString(jdbcExecuteAfterArg.getBeforeArg().getSqlParameters().get(0)));
+            listenerContextManager.clear();
+        }
     }
 }
