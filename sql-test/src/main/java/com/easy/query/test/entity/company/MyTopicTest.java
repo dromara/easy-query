435d6f1c7d6e46640dbe5fc5db0e7c25a2499bb3
xuejiaming <326308290@qq.com>
2024-03-13 19:16:10 +0800

    feat: 1.10.36

---
+++ sql-test/src/main/java/com/easy/query/test/entity/company/MyTopicTest.java
@@ -0,0 +1,19 @@
+package com.easy.query.test.entity.company;
+
+import com.easy.query.core.annotation.EntityProxy;
+import com.easy.query.core.annotation.ValueObject;
+import lombok.Data;
+
+/**
+ * create time 2024/3/13 19:14
+ * 文件说明
+ *
+ * @author xuejiaming
+ */
+@Data
+@EntityProxy
+public class MyTopicTest {
+    private String id;
+    @ValueObject
+    private MyTopicTest2 test2;
+}
---
+++ sql-test/src/main/java/com/easy/query/test/entity/company/MyTopicTest2.java
@@ -0,0 +1,16 @@
+package com.easy.query.test.entity.company;
+
+import lombok.Data;
+
+import java.util.List;
+
+/**
+ * create time 2024/3/13 19:14
+ * 文件说明
+ *
+ * @author xuejiaming
+ */
+@Data
+public class MyTopicTest2 {
+    private List<String> ids;
+}
