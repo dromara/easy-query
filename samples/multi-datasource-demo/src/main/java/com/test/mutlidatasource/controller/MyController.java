589a0ffd45a8e843ba91c2841e4f073a981a60ab
xuejiaming <326308290@qq.com>
2024-03-13 16:12:35 +0800

    feat: 动态数据源demo

--- samples/multi-datasource-demo/src/main/java/com/test/mutlidatasource/controller/MyController.java
+++ samples/multi-datasource-demo/src/main/java/com/test/mutlidatasource/controller/MyController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 /**
  * create time 2024/3/13 15:58
  * 文件说明
@@ -36,5 +38,21 @@ public class MyController {
     @DynamicDataSource("#request.ds")
     public Object test2(@RequestBody MyRequest request){
         return easyMultiEntityQuery.queryable(Topic.class).toList();
+    }
+    @RequestMapping("/test3")
+    public Object test3(){
+        List<Topic> ds2 = easyMultiEntityQuery.executeScope("ds2", eq -> {
+            return eq.queryable(Topic.class).toList();
+        });
+        return ds2;
+    }
+    @RequestMapping("/test4")
+    public Object test4(){
+        try {
+            easyMultiEntityQuery.setCurrent("ds2");
+            return easyMultiEntityQuery.queryable(Topic.class).toList();
+        }finally {
+            easyMultiEntityQuery.clear();
+        }
     }
 }
