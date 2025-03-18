d5a81456ca011283d0e42e7db4b46c8aebdaad98
Gn3po4g <gn3po4g@outlook.com>
2025-03-18 09:24:10 +0800

    修改达梦日期格式

--- sql-db-support/sql-dameng/src/main/java/com/easy/query/dameng/func/DamengDateTimeFormatSQLFunction.java
+++ sql-db-support/sql-dameng/src/main/java/com/easy/query/dameng/func/DamengDateTimeFormatSQLFunction.java
@@ -4,7 +4,7 @@ import com.easy.query.core.expression.parser.core.available.TableAvailable;
 import com.easy.query.core.func.column.ColumnExpression;
 import com.easy.query.core.func.def.AbstractExpressionSQLFunction;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -68,106 +68,139 @@ public class DamengDateTimeFormatSQLFunction extends AbstractExpressionSQLFuncti
                 case "HH:mm:ss":
                     return "TO_CHAR({0},'HH24:MI:SS')";
             }
-            format = replaceFormat(format);
+            return getReplacedFormats(format);
 
-            String[] argsFinds = {"YYYY", "YY", "%_a1", "%_a2", "%_a3", "%_a4", "SS", "%_a5"};
-            String[] argsSpts = format.split("(M|d|H|hh|h|m|s|t)");
-
-            for (int a = 0; a < argsSpts.length; a++) {
-                switch (argsSpts[a]) {
-                    case "M":
-                        argsSpts[a] = "LTRIM(TO_CHAR({0},'MM'),'0')";
-                        break;
-                    case "d":
-                        argsSpts[a] = "CASE WHEN SUBSTR(TO_CHAR({0},'DD'),1,1) = '0' THEN SUBSTR(TO_CHAR({0},'DD'),2,1) ELSE TO_CHAR({0},'DD') END";
-                        break;
-                    case "H":
-                        argsSpts[a] = "CASE WHEN SUBSTR(TO_CHAR({0},'HH24'),1,1) = '0' THEN SUBSTR(TO_CHAR({0},'HH24'),2,1) ELSE TO_CHAR({0},'HH24') END";
-                        break;
-                    case "hh":
-                        argsSpts[a] = "CASE mod(cast(CASE WHEN SUBSTR(TO_CHAR({0},'HH24'),1,1) = '0' THEN SUBSTR(TO_CHAR({0},'HH24'),2,1) ELSE TO_CHAR({0},'HH24') END as number),12) " +
-                                "WHEN 0 THEN '12' WHEN 1 THEN '01' WHEN 2 THEN '02' WHEN 3 THEN '03' WHEN 4 THEN '04' WHEN 5 THEN '05' WHEN 6 THEN '06' " +
-                                "WHEN 7 THEN '07' WHEN 8 THEN '08' WHEN 9 THEN '09' WHEN 10 THEN '10' WHEN 11 THEN '11' END";
-                        break;
-                    case "h":
-                        argsSpts[a] = "CASE mod(cast(CASE WHEN SUBSTR(TO_CHAR({0},'HH24'),1,1) = '0' THEN SUBSTR(TO_CHAR({0},'HH24'),2,1) ELSE TO_CHAR({0},'HH24') END as number),12) " +
-                                "WHEN 0 THEN '12' WHEN 1 THEN '1' WHEN 2 THEN '2' WHEN 3 THEN '3' WHEN 4 THEN '4' WHEN 5 THEN '5' WHEN 6 THEN '6' " +
-                                "WHEN 7 THEN '7' WHEN 8 THEN '8' WHEN 9 THEN '9' WHEN 10 THEN '10' WHEN 11 THEN '11' END";
-                        break;
-                    case "m":
-                        argsSpts[a] = "CASE WHEN SUBSTR(TO_CHAR({0},'MI'),1,1) = '0' THEN SUBSTR(TO_CHAR({0},'MI'),2,1) ELSE TO_CHAR({0},'MI') END";
-                        break;
-                    case "s":
-                        argsSpts[a] = "CASE WHEN SUBSTR(TO_CHAR({0},'SS'),1,1) = '0' THEN SUBSTR(TO_CHAR({0},'SS'),2,1) ELSE TO_CHAR({0},'SS') END";
-                        break;
-                    case "t":
-                        argsSpts[a] = "RTRIM(TO_CHAR({0},'AM'),'M')";
-                        break;
-                    default:
-                        String argsSptsA = argsSpts[a];
-                        if (argsSptsA.startsWith("'")) {
-                            argsSptsA = argsSptsA.substring(1);
-                        }
-                        if (argsSptsA.endsWith("'")) {
-                            argsSptsA = argsSptsA.substring(0, argsSptsA.length() - 1);
-                        }
-                        if (Arrays.stream(argsFinds).anyMatch(argsSptsA::contains)) {
-                            argsSpts[a] = "TO_CHAR({0},'" + argsSptsA + "')";
-                        } else {
-                            argsSpts[a] = "'" + argsSptsA + "'";
-                        }
-                        break;
-                }
-            }
-
-            if (argsSpts.length == 1) {
-                format = argsSpts[0];
-            } else if (argsSpts.length > 1) {
-                format = "(" + String.join(" || ", Arrays.stream(argsSpts).filter(a -> !a.equals("''")).toArray(String[]::new)) + ")";
-            }
-
-            return format.replace("%_a1", "MM").replace("%_a2", "DD").replace("%_a3", "HH24").replace("%_a4", "MI").replace("%_a5", "AM");
+//            String[] argsFinds = {"YYYY", "YY", "%_a1", "%_a2", "%_a3", "%_a4", "SS", "%_a5"};
+//            String[] argsSpts = format.split("(M|d|H|hh|h|m|s|t)");
+//
+//            for (int a = 0; a < argsSpts.length; a++) {
+//                switch (argsSpts[a]) {
+//                    case "M":
+//                        argsSpts[a] = "LTRIM(TO_CHAR({0},'MM'),'0')";
+//                        break;
+//                    case "d":
+//                        argsSpts[a] = "CASE WHEN SUBSTR(TO_CHAR({0},'DD'),1,1) = '0' THEN SUBSTR(TO_CHAR({0},'DD'),2,1) ELSE TO_CHAR({0},'DD') END";
+//                        break;
+//                    case "H":
+//                        argsSpts[a] = "CASE WHEN SUBSTR(TO_CHAR({0},'HH24'),1,1) = '0' THEN SUBSTR(TO_CHAR({0},'HH24'),2,1) ELSE TO_CHAR({0},'HH24') END";
+//                        break;
+//                    case "hh":
+//                        argsSpts[a] = "CASE mod(cast(CASE WHEN SUBSTR(TO_CHAR({0},'HH24'),1,1) = '0' THEN SUBSTR(TO_CHAR({0},'HH24'),2,1) ELSE TO_CHAR({0},'HH24') END as number),12) " +
+//                                "WHEN 0 THEN '12' WHEN 1 THEN '01' WHEN 2 THEN '02' WHEN 3 THEN '03' WHEN 4 THEN '04' WHEN 5 THEN '05' WHEN 6 THEN '06' " +
+//                                "WHEN 7 THEN '07' WHEN 8 THEN '08' WHEN 9 THEN '09' WHEN 10 THEN '10' WHEN 11 THEN '11' END";
+//                        break;
+//                    case "h":
+//                        argsSpts[a] = "CASE mod(cast(CASE WHEN SUBSTR(TO_CHAR({0},'HH24'),1,1) = '0' THEN SUBSTR(TO_CHAR({0},'HH24'),2,1) ELSE TO_CHAR({0},'HH24') END as number),12) " +
+//                                "WHEN 0 THEN '12' WHEN 1 THEN '1' WHEN 2 THEN '2' WHEN 3 THEN '3' WHEN 4 THEN '4' WHEN 5 THEN '5' WHEN 6 THEN '6' " +
+//                                "WHEN 7 THEN '7' WHEN 8 THEN '8' WHEN 9 THEN '9' WHEN 10 THEN '10' WHEN 11 THEN '11' END";
+//                        break;
+//                    case "m":
+//                        argsSpts[a] = "CASE WHEN SUBSTR(TO_CHAR({0},'MI'),1,1) = '0' THEN SUBSTR(TO_CHAR({0},'MI'),2,1) ELSE TO_CHAR({0},'MI') END";
+//                        break;
+//                    case "s":
+//                        argsSpts[a] = "CASE WHEN SUBSTR(TO_CHAR({0},'SS'),1,1) = '0' THEN SUBSTR(TO_CHAR({0},'SS'),2,1) ELSE TO_CHAR({0},'SS') END";
+//                        break;
+//                    case "t":
+//                        argsSpts[a] = "RTRIM(TO_CHAR({0},'AM'),'M')";
+//                        break;
+//                    default:
+//                        String argsSptsA = argsSpts[a];
+//                        if (argsSptsA.startsWith("'")) {
+//                            argsSptsA = argsSptsA.substring(1);
+//                        }
+//                        if (argsSptsA.endsWith("'")) {
+//                            argsSptsA = argsSptsA.substring(0, argsSptsA.length() - 1);
+//                        }
+//                        if (Arrays.stream(argsFinds).anyMatch(argsSptsA::contains)) {
+//                            argsSpts[a] = "TO_CHAR({0},'" + argsSptsA + "')";
+//                        } else {
+//                            argsSpts[a] = "'" + argsSptsA + "'";
+//                        }
+//                        break;
+//                }
+//            }
+//
+//            if (argsSpts.length == 1) {
+//                format = argsSpts[0];
+//            } else if (argsSpts.length > 1) {
+//                format = "(" + String.join(" || ", Arrays.stream(argsSpts).filter(a -> !a.equals("''")).toArray(String[]::new)) + ")";
+//            }
+//
+//            return format.replace("%_a1", "MM").replace("%_a2", "DD").replace("%_a3", "HH24").replace("%_a4", "MI").replace("%_a5", "AM");
         }
         return "TO_CHAR({0},'YYYY-MM-DD HH24:MI:SS.FF6')";
     }
 
-    protected String replaceFormat(String format) {
-        String pattern = "(yyyy|yy|MM|dd|HH|hh|mm|ss|tt)";
+    protected String getReplacedFormats(String format) {
+        String pattern = "(yyyy|yy|MM|dd|HH|hh|mm|ss|[MdHhmsa]|(?:(?!yyyy|yy|MM|dd|HH|hh|mm|ss|[MdHhmsa]).)+)";
         Pattern r = Pattern.compile(pattern);
         Matcher matcher = r.matcher(format);
-        StringBuffer result = new StringBuffer();
+//        StringBuffer result = new StringBuffer();
+        List<String> results = new ArrayList<>();
         while (matcher.find()) {
             String match = matcher.group(1);
             switch (match) {
                 case "yyyy":
-                    matcher.appendReplacement(result, "YYYY");
+                    results.add("TO_CHAR({0}, 'YYYY')");
+//                    matcher.appendReplacement(result, "YYYY");
                     break;
                 case "yy":
-                    matcher.appendReplacement(result, "YY");
+                    results.add("TO_CHAR({0}, 'YY')");
+//                    matcher.appendReplacement(result, "YY");
                     break;
                 case "MM":
-                    matcher.appendReplacement(result, "%_a1");
+                    results.add("TO_CHAR({0}, 'MM')");
+//                    matcher.appendReplacement(result, "%_a1");
+                    break;
+                case "M":
+                    results.add("TO_CHAR({0}, 'FMMM')");
                     break;
                 case "dd":
-                    matcher.appendReplacement(result, "%_a2");
+                    results.add("TO_CHAR({0}, 'DD')");
+//                    matcher.appendReplacement(result, "%_a2");
+                    break;
+                case "d":
+                    results.add("TO_CHAR({0}, 'FMDD')");
                     break;
                 case "HH":
-                    matcher.appendReplacement(result, "%_a3");
+                    results.add("TO_CHAR({0}, 'HH24')");
+//                    matcher.appendReplacement(result, "%_a3");
+                    break;
+                case "H":
+                    results.add("TO_CHAR({0}, 'FMHH24')");
+                    break;
+                case "hh":
+                    results.add("TO_CHAR({0}, 'HH')");
+                    break;
+                case "h":
+                    results.add("TO_CHAR({0}, 'FMHH')");
                     break;
                 case "mm":
-                    matcher.appendReplacement(result, "%_a4");
+                    results.add("TO_CHAR({0}, 'MI')");
+//                    matcher.appendReplacement(result, "%_a4");
+                    break;
+                case "m":
+                    results.add("TO_CHAR({0}, 'FMMI')");
                     break;
                 case "ss":
-                    matcher.appendReplacement(result, "SS");
+                    results.add("TO_CHAR({0}, 'SS')");
                     break;
-                case "tt":
-                    matcher.appendReplacement(result, "%_a5");
+                case "s":
+                    results.add("TO_CHAR({0}, 'FMSS')");
+//                    matcher.appendReplacement(result, "SS");
+                    break;
+                case "a":
+                    results.add("TO_CHAR({0}, 'AM')");
+//                    matcher.appendReplacement(result, "%_a5");
+                    break;
+                default:
+                    results.add('\'' + match + '\'');
                     break;
             }
         }
-
-        matcher.appendTail(result);
-        return result.toString();
+        return String.join("||", results);
+//        matcher.appendTail(result);
+//        return result.toString();
     }
 
     @Override
